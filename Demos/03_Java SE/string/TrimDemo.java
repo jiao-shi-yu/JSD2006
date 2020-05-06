61872121f792f199ec54d28ab5cb05013e721afc
jiaoshiyu <jiao_shiyu@icloud.com>
2020-05-06 17:16:53 +0800

    String API

---
+++ Demos/03_Java SE/string/TrimDemo.java
@@ -0,0 +1,10 @@
+package string;
+
+public class TrimDemo {
+	public static void main(String[] args) {
+		String str = "  hello      ";
+		System.out.println(str);
+		String trim = str.trim();
+		System.out.println(trim);
+	}
+}
