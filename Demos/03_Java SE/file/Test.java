a05b95612d2d143d1bde93a8310f7e12302ba5fa
jiaoshiyu <jiao_shiyu@icloud.com>
2020-05-06 18:36:05 +0800

    FileFilter

---
+++ Demos/03_Java SE/file/Test.java
@@ -0,0 +1,31 @@
+package file;
+
+import java.io.File;
+import java.io.FileFilter;
+
+/**
+ * 获取当前目录下的所有文件（子项是目录的不要）
+ * @author yuyu
+ *
+ */
+public class Test {
+	public static void main(String[] args) {
+		File dir = new File(".");
+		if (dir.isDirectory()) {
+			FileFilter filter = new FileFilter() {
+
+				@Override
+				public boolean accept(File file) {
+					// TODO Auto-generated method stub
+					return file.isFile();
+				};
+				
+			};
+			File[] data = dir.listFiles(filter);
+			System.out.println(data.length);
+			for (File file : data) {
+				System.out.println(file.getName());
+			}
+		}
+	}
+}
