8b18af2dc738514e7669cd66ed991d31c64c3f35
jiaoshiyu <jiao_shiyu@icloud.com>
2020-05-06 18:35:42 +0800

    dir.delete()

---
+++ Demos/03_Java SE/file/DeleteDirDemo.java
@@ -0,0 +1,20 @@
+package file;
+
+import java.io.File;
+
+/**
+ * 删除一个目录
+ * @author yuyu
+ *
+ */
+public class DeleteDirDemo {
+	public static void main(String[] args) {
+		File dir = new File("demo");
+		if (dir.exists()) {
+			dir.delete();
+			System.out.println("成功删除");
+		} else {
+			System.out.append("目录不存在");
+		}
+	}
+}
